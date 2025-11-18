d590a66e52749e1f461cd831b004d5db6b837b50
GIKExe <i.gortolomei@yandex.ru>
2025-11-18 13:24:26 +0300

    3 лаба фикс 5 задачи

--- Информатика/лаба 3/5.c
+++ Информатика/лаба 3/5.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include <math.h>
-
+unsigned short read() {
+  unsigned short x = 0, z;
+  for (int i=32768; i>0; i/=2) {
+    scanf("%1hu", &z); x += i * z; }
+  return x;
+}
 int main() {
-  int mx, my, mz;
-  unsigned char ex, ey, sx, sy;
-  unsigned short x=0, y=0, z, i;
-  for (i=32768; i>0; i/=2) {scanf("%1hu", &z); x+=i*z;}
-  for (i=32768; i>0; i/=2) {scanf("%1hu", &z); y+=i*z;}
-  sx = x / 32768 % 2;
-  sy = y / 32768 % 2;
-  ex = x / 1024 % 32;
-  ey = y / 1024 % 32;
-  mx = x % 1024 + 1024;
-  my = y % 1024 + 1024;
-  if (ex>ey) {
-    my/=pow(2, ex-ey); ey = ex;
-  } else {
-    mx/=pow(2, ey-ex); ex = ey;
+  unsigned short x=read(), y=read(), z, i;
+  char sx = x / 32768, sy = y / 32768;
+  char ex = x / 1024 % 32, ey = y / 1024 % 32;
+  int mx = x % 1024 + 1024, my = y % 1024 + 1024;
+  if (ex>ey) { my/=pow(2, ex-ey); ey=ex; }
+  else { mx/=(2, ey-ex); ex=ey; }
+  if (sx) mx = -mx;
+  if (sy) my = -my;
+  int mz = mx + my;
+  char sz = mz<0;
+  if (sz) mz = -mz;
+  printf("%d", sz);
+  if (!mz) {ex = 0;}
+  else {
+    while (mz < 1024) { mz*=2; ex--; }
+    while (mz >= 2048) { mz/=2; ex++; }
   }
-  if (sx>0) mx = -mx;
-  if (sy>0) my = -my;
-  mz = mx + my;
-  if (mz<0) {mz=-mz; printf("1");}
-  else printf("0");
-  
-  while (mz/1024 != 1 && mz!=0) {
-    if (mz/1024==0) { mz/=2; ex++; }
-    else { mz*=2; ex--; }
-  }
-  for (i=32; i>0; i/=2) printf("%d", ex/i%2);
-  for (i=1024; i>0; i/=2) printf("%d", mz/i%2);
+  for (i=16; i>0; i>>=1) printf("%d", (ex>>(int)log2(i))&1);
+  for (i=512; i>0; i>>=1) printf("%d", (mz>>(int)log2(i))&1);
   return 0;
 }
