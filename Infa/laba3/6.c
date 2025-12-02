0bc0cdef63b0442c10664262ef778b6bb20ce019
GIKExe <i.gortolomei@yandex.ru>
2025-12-02 12:56:24 +0300

    исправление 6 задачи

--- Infa/laba3/6.c
+++ Infa/laba3/6.c
@@ -3,27 +3,28 @@
 
 int main() {
   float x;
-  int gs, ms, es,  sx, ex, n, digit;
+  int gs, ms, es,  sx, ex, n, digit, sex;
   scanf("%f %d %d", &x, &gs, &ms);
-  es = gs - ms - 1;
+  es = gs - ms - 2;
   sx = 0;
   if (x < 0) {sx=1; x=-x;}
   printf("%d", sx);
-  ex = pow(2, es-1) - 1;
-  if (x == 0) ex = 0;
-  else {
-    while (x >= 2) {x/=2; ex++;}
-    while (x < 1) {x*=2; ex--;}
-    x = x - 1;
-  }
-  for (n=pow(2,es-1); n>0; n/=2) {
-    digit = ex/n%2;
-    printf("%d", digit);
+  ex = 0;
+  if (x != 0) {
+    while (x >= 1) {x/=2; ex--;}
+    while (x < 0.5) {x*=2; ex++;}
   }
   for (n=pow(2,ms-1); n>0; n/=2) {
     x*=2;
     digit = (int) x;
     x -= digit;
+    printf("%d", digit);
+  }
+  sex = 0;
+  if (ex < 0) {sex=1; ex=-ex;}
+  printf("%d", sex);
+  for (n=pow(2,es-1); n>0; n/=2) {
+    digit = ex/n%2;
     printf("%d", digit);
   }
   return 0;
