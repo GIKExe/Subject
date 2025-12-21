27e684f13f89bc1bb5f9635531575c3f7e432381
GIKExe <i.gortolomei@yandex.ru>
2025-12-21 06:00:04 +0300

    начало в 4 лабе по проге

--- Proga/laba4/main.pas
+++ Proga/laba4/main.pas
@@ -1,5 +1,31 @@
-Program Main;
+﻿program ShowKeyCodes;
+
+uses
+  Keyboard;
+
+var
+  Key: TKeyEvent;
+  KeyCode: Word;
 begin
-	write('1 ');
-	write('ASS');
+  InitKeyboard;
+  repeat
+    Key := GetKeyEvent;
+    Key := TranslateKeyEvent(Key);
+    KeyCode := GetKeyEventCode(Key);
+
+    // writeln('Код клавиши: ', KeyCode);
+    if KeyCode = 283 then // ESC
+    begin
+      writeln('Выход...'); Break;
+    end else if KeyCode = 65313 then begin
+			writeln('вверх');
+		end else if KeyCode = 65319 then begin
+			writeln('вниз')
+		end;
+
+		// Код клавиши: 65313 ^
+		// Код клавиши: 65319 v
+
+  until false;
+  DoneKeyboard;
 end.
