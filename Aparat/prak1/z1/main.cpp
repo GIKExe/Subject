43831028de2d68eb519a8618112222792fe1e60b
GIKExe <i.gortolomei@yandex.ru>
2026-03-03 22:06:09 +0300

    Апарат. 1 задание Практика

---
+++ Aparat/prak1/z1/main.cpp
@@ -0,0 +1,18 @@
+#define FIRST_LED_PIN 2
+#define LAST_LED_PIN 11
+
+void setup() {
+	for (int pin = FIRST_LED_PIN; pin <= LAST_LED_PIN; ++pin)
+		pinMode(pin, OUTPUT);
+}
+
+void loop() {
+	unsigned int ms = millis();
+	int i =  + (ms / 120) % 10;
+  	if (i > 4) i = 9 - i;
+	digitalWrite(FIRST_LED_PIN + i, HIGH);
+  	digitalWrite(FIRST_LED_PIN + (9 - i), HIGH);
+	delay(10);
+	digitalWrite(FIRST_LED_PIN + i, LOW);
+    digitalWrite(FIRST_LED_PIN + (9 - i), LOW);
+}
