80802d1016bd99ce10a17d4029760d4a51a440a0
GIKExe <i.gortolomei@yandex.ru>
2026-03-09 12:24:36 +0300

    Алгосы. 5 задание решено

---
+++ Algo/laba1/5.py
@@ -0,0 +1,27 @@
+# https://codeforces.com/contest/1598/problem/C
+from sys import stdin
+from collections import defaultdict
+
+def main():
+	t = int(stdin.readline())
+	for _ in range(t):
+		n = int(stdin.readline())
+		a = list(map(int, stdin.readline().split()))
+		s = sum(a)
+		
+		if (2 * s) % n != 0:
+			print(0)
+			continue
+			
+		target = (2 * s) // n
+		count = defaultdict(int)
+		ans = 0
+		
+		for x in a:
+			need = target - x
+			ans += count[need]
+			count[x] += 1
+		print(ans)
+
+if __name__ == "__main__":
+	main()
