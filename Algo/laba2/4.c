6f5616ea5d2b6a261e9c2c012b00e9d5b1d9ed3e
GIKExe <72767917+GIKExe@users.noreply.github.com>
2026-03-24 14:44:42 +0300

    Алгосы 2 задачи готовы

---
+++ Algo/laba2/4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	int n, m; scanf("%d %d", &n, &m);
+	int g[n+1], c = 0;
+	for (int i = 0; i < n+1; i++) 
+		g[i] = 0;
+
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		scanf("%d %d", &x, &y);
+		g[x]++;
+		g[y]++;
+	}
+
+	bool bus = true;
+	bool star = false;
+	for (int i = 1; i < n+1; i++) {
+		if (g[i] == 1) c++;
+		if (g[i] > 2) bus = false;
+		if (g[i] == m) star = true;
+	}
+		
+	if (star && c == n-1) {
+		printf("star topology");
+	} else if (bus && c == 0) {
+		printf("ring topology");
+	} else if (bus && c == 2) {
+		printf("bus topology");
+	} else {
+		printf("unknown topology");
+	}
+	return 0;
+}
