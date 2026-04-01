c99f3fcb927c25c8e1951a9072049de2a2b51de4
GIKExe <72767917+GIKExe@users.noreply.github.com>
2026-04-01 10:26:04 +0300

    Апарат 2 задание 7

--- Aparat/laba2/7.c
+++ Aparat/laba2/7.c
@@ -5,43 +5,47 @@ void setup() {
 	for (int i = 0; i < 7; i++) digitalWrite(i+2+5, 1);
 }
 
-const unsigned char nabor[2][32][2] = {
+const unsigned char nabor[2][20][2] = {
 	{
-		{0, 6},
-		{0, 5},
-		{0, 4},
-		{0, 3},
-		{0, 2},
-		{0, 1},
-		{0, 0},
+		{0, 6}, // #####
+		{0, 5}, // #
+		{0, 4}, // #
+		{0, 3}, // #
+		{0, 2}, // #
+		{0, 1}, // #
+		{0, 0}, // #
 		{1, 0},
-		{2, 0},
+		{2, 0}, // 11 точек
 		{3, 0},
 		{4, 0},
 	},
 	{
-		{4, 0},
-		{3, 0},
-		{2, 0},
-		{1, 0},
-		{0, 0},
-		{0, 1},
-		{0, 2},
+		{4, 0}, // ##### 
+		{3, 0}, // #
+		{2, 0}, // #
+		{1, 0}, // ####
+		{0, 0}, //     #
+		{0, 1}, //     #
+		{0, 2}, // ####
 		{0, 3},
-		{1, 3},
+		{1, 3}, // 17 точек
 		{2, 3},
 		{3, 3},
-		{4, 3},
 		{4, 4},
 		{4, 5},
-		{3, 5},
-		{2, 5},
-		{1, 5},
-		{0, 5},
+		{3, 6},
+		{2, 6},
+		{1, 6},
+		{0, 6},
 	}
 };
 
-void pix(int a, int b) {
+bool flag = false;
+int index = 0;
+
+void pix() {
+	int a = nabor[(int) flag][index][0];
+	int b = nabor[(int) flag][index][1];
 	digitalWrite(a+2, 1);
 	digitalWrite(b+2+5, 0);
 	delay(1);
@@ -49,21 +53,17 @@ void pix(int a, int b) {
 	digitalWrite(b+2+5, 1);
 }
 
-bool flag = false;
 unsigned long long time = millis();
-int index = 0;
 void loop() {
 	if (millis() - time > 1000) {
 		flag = !flag;
 		time = millis();
 	}
-		
-	pix(nabor[(int) flag][index][0], nabor[(int) flag][index][1]);
+	pix();
 	index++;
 	if (flag) {
-		if (index > 17) index = 0;
+		if (index >= 17) index = 0;
 	} else {
-		if (index > 11) index = 0;
-	}
-	
+		if (index >= 11) index = 0;
+	}	
 }
