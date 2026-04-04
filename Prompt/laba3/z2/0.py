95701d163d90b6f2783a631de379ddc017325eed
GIKExe <i.gortolomei@yandex.ru>
2026-04-04 15:22:49 +0300

    Промпт 3 задание 2

---
+++ Prompt/laba3/z2/0.py
@@ -0,0 +1,5 @@
+def f(a, b, c, d, e):
+	b = list(zip(*b))
+	g = [[sum(a[k] * b[k][j] for k in range(len(a))) + d[j] for j in range(len(b[0]))]]
+	h = [[(1 / (1 + 2.7182818284 ** -x)) for x in y] for y in g][0]
+	return [sum(h[k] * c[k][j] for k in range(len(h))) + e[j] for j in range(len(c[0]))]
