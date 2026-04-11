31732995d0df530b869fb2244e60541c6337a8d6
GIKExe <72767917+GIKExe@users.noreply.github.com>
2026-04-11 14:17:44 +0300

    Апарат 2 задание 3 (незак)

---
+++ Aparat/laba2/3.c
@@ -0,0 +1,28 @@
+#include "Arduino.h"
+#include <stdbool.h>
+
+#define RESET_PIN 2
+#define CLICK_PIN 3
+
+void setup() {
+	Serial.begin(9600);
+	for (int i = 0; i < 3; i++) pinMode(i+2, OUTPUT);
+	digitalWrite(RESET_PIN, 1);
+	delay(10);
+	digitalWrite(RESET_PIN, 0);
+	for (int i = 0; i < 10; i++) {
+		digitalWrite(CLICK_PIN, 1);
+		delay(5);
+		digitalWrite(CLICK_PIN, 0);
+		delay(500);
+	}
+}
+
+void loop() {
+
+}
+
+
+
+
+// https://pandia.org/text/79/290/images/image006_48.gif
