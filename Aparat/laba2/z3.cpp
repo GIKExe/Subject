4cfdd308dae12131d0edbd5544df122d2cf73b0d
GIKExe <i.gortolomei@yandex.ru>
2026-04-15 22:07:44 +0300

    Апарат 2 задание 3 исправлено

--- Aparat/laba2/z3.cpp
+++ Aparat/laba2/z3.cpp
@@ -1,56 +1,28 @@
 #include "Arduino.h"
 #include <stdbool.h>
 
-#define RESET_PIN 2
-#define CLOCK_PIN_H 3
-#define CLOCK_PIN_L 4
-
+#define RESET_PIN 12
+#define CLOCK_PIN 11
 typedef unsigned char u8;
 
-bool is_harshad_number(u8 num) {
-  u8 x = num % 10 + num / 10 % 10;
-  return num % x == 0;
-}
-
-void ping(u8 pin) {
-  digitalWrite(pin, 1);
-  digitalWrite(pin, 0);
-}
-
-bool checker(u8 pin, u8 num, u8 *x) {
-  if ((*x) == num) return false;
-  (*x) = ((*x) + 1) % 10;
-  digitalWrite(pin, 1);
-  return true;
+bool is_not_harshad(u8 number) {
+  if (number == 0) return true;
+  u8 x = number % 10 + number / 10 % 10;
+  return number % x != 0;
 }
 
 void setup() {
-  for (int i = 0; i < 3; i++) pinMode(i+2, OUTPUT);
-  ping(RESET_PIN);
+  pinMode(RESET_PIN, OUTPUT);
+  pinMode(CLOCK_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, 1);
+  digitalWrite(RESET_PIN, 0);
 }
 
-u8 num = 1;
-u8 num_l = 0, num_h = 0;
+u8 number = 0;
 void loop() {
-  if (!is_harshad_number(num)) {
-    num++;
-    return;
-  }
-
-  if (num > 99) {
-    ping(RESET_PIN);
-    num = 1;
-    num_l = 0;
-    num_h = 0;
-  }
-
-  if (checker(CLOCK_PIN_L, num % 10, &num_l)
-   || checker(CLOCK_PIN_H, (num / 10) % 10, &num_h)) {
-    digitalWrite(CLOCK_PIN_L, 0);
-    digitalWrite(CLOCK_PIN_H, 0);
-    return;
-  }
-
+  number = (number + 1) % 100;
+  digitalWrite(CLOCK_PIN, 1);
+  digitalWrite(CLOCK_PIN, 0);
+  if (is_not_harshad(number)) return;
   delay(1500);
-  num++;
 }
