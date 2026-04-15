86f3f6f29ef7db022e857d077a6791a69a4a9029
GIKExe <72767917+GIKExe@users.noreply.github.com>
2026-04-15 15:49:07 +0300

    Апарат 2 практика задание 2

---
+++ Aparat/prak2/z2.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+
+#include <LiquidCrystal.h>
+
+// пины на МЭЛТ идут 
+// с 14 по 1, 16, 15.
+#define RS_PIN 2
+#define E_PIN 3
+#define D4 8
+#define D5 9
+#define D6 10
+#define D7 11
+
+LiquidCrystal lcd(RS_PIN, E_PIN, D4, D5, D6, D7);
+
+void setup() {
+  lcd.begin(16, 2);
+	lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("\xA5" "BAH");
+  lcd.setCursor(0, 1);
+  lcd.print("\xA1OPTO\xA7OME\xA6");
+}
+
+void loop() {}
